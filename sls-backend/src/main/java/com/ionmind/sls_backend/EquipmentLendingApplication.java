434f23d928955253f55506ad565448b370068296
IonMind <kumarakc55@example.com>
2025-11-10 09:28:23 +0000

    Initial commit with initial project skeleton

---
+++ sls-backend/src/main/java/com/ionmind/sls_backend/EquipmentLendingApplication.java
@@ -0,0 +1,11 @@
+package com.ionmind.sls_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentLendingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentLendingApplication.class, args);
+    }
+}
